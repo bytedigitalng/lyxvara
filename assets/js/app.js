f6d0190720d2f8854e84d25430002ac37f3f1589
Obinna Ukpabi <askphntm@gmail.com>
2020-05-14 08:36:53 +0100

    updated all pages

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,8 @@
 $(function () {
+  setTimeout(function () {
+    $("body").addClass("loaded");
+  }, 2000);
+
   // Navigation animation starts
   $(window).on("scroll", function () {
     if ($(window).scrollTop() > 200) {
@@ -18,4 +22,10 @@ $(function () {
     $(this).find(".article-img-holder").css("transform", "scale(1)");
   });
   //zoom background image
+
+  // alert(window.location.pathname);
+
+  $("#headerinc").load("includes/header.html");
+  $("#newsletterinc").load("includes/newsletter.html");
+  $("#footerinc").load("includes/footer.html");
 });
